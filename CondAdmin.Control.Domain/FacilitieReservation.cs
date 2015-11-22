e3bee490de629b04da4352985e40e7cd4dc98cb4
Riasel <arielcj@hotmail.com>
2015-11-22 19:45:09 -0300

    Classe para reserva de instalação

---
+++ CondAdmin.Control.Domain/FacilitieReservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondAdmin.Control.Domain
+{
+    class FacilitieReservation
+    {
+        public Facilitie reservedFacilitie { get; set; }
+        public DateTime reservationDateTime { get; set; }
+        public int timePeriod { get; set; }
+        public string reservationComment { get; set; }
+
+    }
+}
