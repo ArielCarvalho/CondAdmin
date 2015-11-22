724ca8bc873892b58916f24b1c2d376d21ab550e
Riasel <arielcj@hotmail.com>
2015-11-22 18:56:11 -0300

    Classe de Ocorrências

---
+++ CondAdmin.Control.Domain/Occurrence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondAdmin.Control.Domain
+{
+    class Occurrence
+    {
+        public enum occurrenceType
+        {
+            suggestion,
+            complaint,
+            other
+        }
+
+        public int occurrenceId { get; set; }
+        public string occurrenceDs { get; set; }
+        public occurrenceType typeOfOccurrence { get; set; }
+        public int senderId { get; set; }
+
+    }
+}
